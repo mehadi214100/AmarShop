4f484a44739fc18020ca96c315b2293fc285bce4
mehadi <mehadi214100@gmail.com>
2025-11-12 19:20:26 +0600

    filter option added

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path("",views.home,name="home"),
     path("all_products/",views.all_products,name="all_products"),
-    path("all_products/<slug:category>/",views.all_products,name="category_products"),
+    path("all_products/<slug:category_wase>/",views.all_products,name="category_products"),
     path("product_details/<slug:product_name>/",views.product_details,name="product_details"),
     path("search_product/",views.search_product,name="search_product"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -12,14 +12,46 @@ def home(request):
     return render(request,"shop/home.html",context)
 
 
-def all_products(request,category=None):
+def all_products(request,category_wase=None):
+
+    products = Product.objects.select_related('category').filter(
+        available=True,
+        category__is_available=True
+    )
+
+    if category_wase:
+        products = Product.objects.select_related('category').filter(category__slug=category_wase)
+    
+
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    sort_by = request.GET.get('sort_by')
+    category = request.GET.get('category')
+
     if category:
-        products = Product.objects.select_related('category').filter(available=True,category__is_available=True,category__slug=category)
+        if category != "all":
+            products = Product.objects.select_related('category').filter(category__slug=category)
+
+    if price_min:
+        products = products.filter(price__gte=price_min)
+    if price_max:
+        products = products.filter(price__lte=price_max)
+
+
+    if sort_by == 'low_to_high':
+        products = products.order_by('price')
+    elif sort_by == 'high_to_low':
+        products = products.order_by('-price')
+    elif sort_by == 'new_arrivals':
+        products = products.order_by('-created_at')
     else:
-        products = Product.objects.select_related('category').filter(available=True,category__is_available=True)
+        products = products.order_by('-created_at')
+
+    categories = Category.objects.all().filter(is_available=True)
 
     context = {
         "products":products,
+        'categories':categories
     }
     return render(request,"shop/product_list.html",context)
 
